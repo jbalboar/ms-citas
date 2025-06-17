3c1bd7cb775a3441e1d2bab16aa20154fc6fa9cb
jbalboar <jbalboa.fiis@gmail.com>
2025-06-17 18:34:33 -0500

    primer commit

---
+++ src/main/java/com/sunat/citas/MsCitasApplication.java
@@ -0,0 +1,13 @@
+package com.sunat.citas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sunat/citas/MsCitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunat.citas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
